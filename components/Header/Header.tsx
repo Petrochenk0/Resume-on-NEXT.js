231ba6c23bcfd5756cef4f0ce71c78b5cdb2beb2
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2023-11-29 10:35:55 +0300

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,10 +4,6 @@ import styles from './styles.module.css';
 export const Header = () => {
   return (
     <header className={styles.header__container}>
-      {/* В отличии от React как я знаю, тут мы используем href вместо to как в роутинге */}
-      {/* Мы всё прописываем в Header компоненте
-      Так как мы указываем в Link href="/"
-      NextJS автоматически определяет наши папки и теперь он и отображает */}
       <Link href="/" className={styles.header__link}>
         Home
       </Link>
